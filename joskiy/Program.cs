44c9f3ff9834f5e5de747c5ec331e74136ab982f
Daniil <klementevdaniil@gmail.com>
2022-01-31 14:00:29 +0200

    first

---
+++ joskiy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace joskiy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
